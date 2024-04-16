ca4f7469a27979f9b6050595d92ddc80c6403220
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-16 18:56:44 +0200

    feat: le-12-fatiche-di-javascript

--- le-12-fatiche-di-javascript/app.js
+++ le-12-fatiche-di-javascript/app.js
@@ -1,51 +1,59 @@
-const input = document.querySelector('input');
-const array = null;
+// * 1- Che cosa fa questo codice?
+//  stampa in pagina l'elemento inserito dall'utente più volte
+// * 2- Sono presenti errori di sintassi?
+//
+// * 3- Sono presenti errori logici?
+//
 
-input.addEventListener('keypress', function(event) {
+const input = document.querySelector("input"); // input va scritto tra doppi apici
+const array = []; // per definire l'array vuoto, inserisco le parentesi quadrate
 
-    if (event.code != 'Enter')    return;
-    if (input.value.length == '') return;
+input.addEventListener("keypress", function (event) {
+  if (event.code != "Enter") return;
+  if (input.value.length == "") return;
 
-    array.add(input.value);
+  array.push(input.value); // per aggiungere l'elemento all'array uso push
 
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    li.text = input.value;
-    document.querySelector('ul').push(li);
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.innerText = input.value; // per modificare il testo nell'elemento uso innerText
+  document.querySelector("ul").append(li); // per aggiungere l'elemento creato al DOM uso append
 
-    let counter = '';
-    let item    = array[0];
-    const max   = 1;
-    const elems = [];
+  let counter = 0; // inizializzo il contatore
+  let item = array[0];
+  let max = 1;
+  const elems = [];
 
-    for (let i = 0; i < array; i++) {
-        let val = array[i];
+  for (let i = 0; i < array.length; i++) {
+    // manca .length per scorrere fino all'ultimo elemento
+    let val = array[i];
 
-        if (!elems[val]) {
-            elems[val] = 1;
-        } else {
-            elems[val]++;
-        }
-
-        for (let j = i; j < array.length; i++) {
-            if (array[i] == array[j]) {
-                counter++;
-                if (max < counter)
-                max  = counter;
-                item = array[i];
-            }
-        }
-
-        counter = 0;
+    if (!elems[val]) {
+      elems[val] = 1;
+    } else {
+      elems[val]++;
     }
 
-    const alert = document.getElementsByClassName('alert');
+    for (let j = i; j < array.length; j++) {
+      // correggo l'incremento dell'index indicando con j++ anzichè i++
+      if (array[i] == array[j]) {
+        counter++;
+        if (max < counter) {
+          max = counter;
+          item = array[i];
+        }
+      }
+    }
+    counter = 0;
+  }
 
-    alert.classList.remove('d-none');
-    alert.classList.add('d-flex');
+  const alert = document.getElementsByClassName("alert")[0]; // catturo il primo indice
 
-    alert.querySelector('span:first-child').innerText = item;
-    alert.querySelector('span:last-child').innerText = max;
+  alert.classList.remove("d-none");
+  alert.classList.add("d-flex");
 
-    console.log('${item} trovato ${max} volte');
+  alert.querySelector("span:first-child").innerText = item;
+  alert.querySelector("span:last-child").innerText = max;
+
+  console.log(`${item} trovato ${max} volte`); // per template literal va usato il backtick
 });
