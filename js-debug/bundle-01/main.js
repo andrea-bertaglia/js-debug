179310a42dd62367982b11611c913bed743ab64f
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-16 16:31:43 +0200

    feat: bundle-01

--- js-debug/bundle-01/main.js
+++ js-debug/bundle-01/main.js
@@ -15,6 +15,13 @@ for (let i = 0; i > 5; i++) {
     console.log(i);
 }
 
+// * 1- Che cosa fa questo codice?
+// effettua un ciclo for infinito in quanto la condizione di fine ciclo è indefinita (i maggiore di 5, dato che incrementa sempre di 1 con i++ non c'è una fine)
+// * 2- Sono presenti errori di sintassi?
+// no
+// * 3- Sono presenti errori logici?
+// c'è un errore di logica (tranne che quando i = 0,1,2,3,4 la condizione i > 5 sarà sempre vera con let i = 0)
+
 
 // ESERCIZIO 2
 function addIfEven(num) {
@@ -24,6 +31,13 @@ function addIfEven(num) {
     return num;
 }
 
+// * 1- Che cosa fa questo codice?
+// la funzione dovrebbe ritornare un numero addizionato di 5, se il numero originario è pari
+// * 2- Sono presenti errori di sintassi?
+// si in quanto viene usato = per la verifica, ma non è possibile in quanto = è un operatore di assegnazione 
+// * 3- Sono presenti errori logici?
+// se l'intenzione è ritornare un numero SOLO se è pari, non ci dovrebbe essere l'ultima riga in quanto ritorna il numero di partenza anche nel caso in cui sia dispari
+
 
 // ESERCIZIO 3
 function loopToFive() {
@@ -31,6 +45,13 @@ function loopToFive() {
         console.log(i);
     }
 }
+
+// * 1- Che cosa fa questo codice?
+// la funzione dovrebbe stampare in console tutti i numeri compresi tra 0 e 4, tramite un ciclo for
+// * 2- Sono presenti errori di sintassi?
+// si, gli argomenti del ciclo for devono essere separati con ";" mentre qui viene usata la virgola ","
+// * 3- Sono presenti errori logici?
+// no (se l'intenzione è arrivare a 5, allora in questo caso c'è un errore perchè andrebbe messo <=)
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
@@ -45,3 +66,17 @@ function displayEvenNumbers() {
     }
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+// * 1- Che cosa fa questo codice?
+// la funzione ritorna i numeri pari dell'array numbers
+// * 2- Sono presenti errori di sintassi?
+// si, gli errori sono
+//  1. operatore di assegnazione per let i = 0 dentro il ciclo for
+//  2. c'è il simbolo ";" dopo i++, ma non ci va nell'ultimo argomento
+//  3. nella condizione numbers % 2 c'è l'operatore di assegnazione "="
+//  4. dopo la condizione if c'è il ";" prima della parentesi graffa che non va messo
+// * 3- Sono presenti errori logici?
+// si, gli errori sono
+//  1. per pushare il numero pari nell'array, bisogna scrivere numbers [i] per prendere il valore, mentre qui viene pushato il valore dell'indice
+//  2. per mostrare il risultato atteso [2,4,6,8] la funzione dovrebbe essere eseguita dentro console.log()
+//  3. per ciclare su tutto l'array bisogna mettere la condizione i < numbers.length altrimenti come è scritto qui si ferma all'indice 6
